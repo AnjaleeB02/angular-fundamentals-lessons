ce6e82122287aa08d997d4c9e3ea31eba39555bb
AnjaleeB02 <anjaleeb02@gmail.com>
2026-03-12 11:01:40 +0530

    update : using if-else statements with classes

--- projects/04-control-flow-if/src/app/app.component.ts
+++ projects/04-control-flow-if/src/app/app.component.ts
@@ -11,15 +11,22 @@ import { AccountInfo } from './account-info';
       <section class="membership-info">
         <p>
           <!-- name -->
-          {{ '' }}
+          {{ account.name }}
         </p>
-        <p>Valid Thru: {{ '' }}</p>
-        <p>CVV: {{ '' }}</p>
+        <p>Valid Thru: {{ account.validThru }}</p>
+        <p>CVV: {{ account.CVV }}</p>
         <p>
+
           <!-- membership status -->
+          <!-- we can also use switch case -->
+          @if (account.membershipStatus === 'gold') {
           <span class="badge gold">Gold</span>
+          }@else if (account.membershipStatus === 'platinum') {
           <span class="badge platinum">Platinum</span>
+          }@else {
           <span class="badge silver">Silver</span>
+          }
+
         </p>
       </section>
     </article>
@@ -28,8 +35,8 @@ import { AccountInfo } from './account-info';
 })
 export class AppComponent {
   account: AccountInfo = {
-    name: 'Melisa Evan',
-    membershipStatus: 'gold',
+    name: 'Anjalee Balasooriya',
+    membershipStatus: 'platinum',
     validThru: '12/2022',
     CVV: '123',
   };
